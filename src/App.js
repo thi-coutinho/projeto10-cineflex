08f58937143d62b8461d103030710d104b642bd1
thi-coutinho <thi.coutinho.santos@gmail.com>
2022-12-09 10:31:18 -0300

    feat:routes implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import { BrowserRouter, Routes,Route } from "react-router-dom";
+import MoviePage from "./pages/MoviePage";
+import SeatPage from "./pages/SeatPage";
+import SessionPage from "./pages/SessionPage";
+import SuccessPage from "./pages/SuccessPage";
 
 export default function App() {
   return (
-  <>App</>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<MoviePage/>}/>
+      <Route path="/sessoes/:idSession" element={<SessionPage/>}/>
+      <Route path="/assentos/:idSeat" element={<SeatPage/>}/>
+      <Route path="/sucesso" element={<SuccessPage/>}/>
+    </Routes>
+  </BrowserRouter>
   );
 }
---
+++ src/pages/SeatPage.js
@@ -0,0 +1,5 @@
+export default function SeatPage() {
+    return (
+        <>SeatPage</>
+    )
+}
